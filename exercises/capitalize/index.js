1e0fd495125384c4d01b6f7edc529e1a8da83616
crispinamuriel <crispinamuriel@gmail.com>
2019-08-05 13:26:01 -0400

    steps done

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,68 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// function capitalize(str) {
+//     const resultArr = [];
+//     const strArr = str.split(' ');
+//     strArr.forEach((word) => {
+//         const firstLetter = word.slice(0, 1).toUpperCase();
+//         word = firstLetter + word.slice(1);
+//         resultArr.push(word);
+//     });
+//     return resultArr.join(' ');
+// }
 
+// function capitalize(str) {
+//     const words = [];
+//     const splitStr = str.split(' ');
+//     splitStr.forEach((word) => {
+//         const firstLetter = word[0].toUpperCase();
+//         word = firstLetter + word.slice(1);
+//         words.push(word);
+//     })
+//     return words.join(' ');
+// }
+
+// function capitalize(str) {
+//     let result = '';
+//     for (let i = 0; i < str.length; i++) {
+//         const el = str[i];
+//         if (i === 0) {
+//             result = result + el.toUpperCase();
+//         }
+//         if(el === ' ') {
+//             result = result + ' ' + str[i + 1].toUpperCase();
+//         } else if (i !== 0 && str[i-1] !== ' ') {
+//             result = result + el;
+//         }
+//     }
+//     return result;
+// }
+
+// function capitalize(str) {
+//     let result = '';
+//     for (let i = 0; i < str.length; i++) {
+//         if (i === 0){
+//             result += str[i].toUpperCase();
+//         } else if (str[i-1] === ' ') {
+//             result += str[i].toUpperCase();
+//         } else {
+//             result += str[i];
+//         }
+//     }
+//     return result;
+// }
+
+function capitalize(str) {
+    let result = str[0].toUpperCase();
+    for (let i = 1; i < str.length; i++) {
+        if (str[i-1] === ' '){
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
+        }
+    }
+    return result;
+}
+capitalize('a short sentence')
 module.exports = capitalize;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,71 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//     for(let i = 1; i <= n; i++) {
+//         if(i === n) {
+//             let step = ''
+//             let count = n;
+//             while (count){
+//                 step += '#';
+//                 count--;
+//             }
+//             console.log(step);
+//         } else {
+//             let step = '';
+//             let count = i;
+//             let count2 = n - i;
+//             while (count) {
+//                 step += '#';
+//                 count--;
+//             }
+//             while (count2) {
+//                 step += ' ';
+//                 count2--; 
+//             }
+//             console.log(step);
+//         }
+//     }
+// }
+//psuedo code solution
 
+// function steps(n) {
+//     for (let i = 0; i < n; i++) {
+//         let stair = '';
+//         for (let j = 0; j < n; j++) {
+//             if (j <= i) {
+//                 stair += '#';
+//             } else {
+//                 stair += ' ';
+//             }
+//         }
+//         console.log(stair);
+//     }
+// }
+//solution
+/* 
+from 0 to n (iterate through rows)
+    create an empty string, 'stair'
+    from 0 to n (iterate through columns)
+        if the current column is equal to or less than the current row
+            add a '#' to 'stair'
+        else
+            add a space to 'stair'
+    console.log(stair);
+*/
+function steps(n) {
+    for (let row = 0; row < n; row++) {
+        let stair = '';
+        for (let column = 0; column < n; column++) {
+            if(column <= row) {
+                stair += '#';
+            } else {
+                stair += ' '
+            }
+        }
+        console.log(stair);
+    }
+}
+
+steps(3);
 module.exports = steps;
