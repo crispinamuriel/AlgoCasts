76986d7622f2b677e06768992a9e15f538bee885
crispinamuriel <crispinamuriel@gmail.com>
2019-08-07 15:22:31 -0400

    matrix iterative solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,18 +14,75 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-    for (let row = 0; row < n; row++) {
-        let level = '';
-        for (let column = 0; column < (n * 2 - 1); column++) {
-            if (column >= n - row - 1 && column <= n + row - 1) {
-                level += '#';
-            } else {
-                level += ' ';
-            }
-        }
-        console.log(level);
+// function pyramid(n) {
+//     for (let row = 0; row < n; row++) {
+//         let level = '';
+
+//         for (let column = 0; column < (n * 2 - 1); column++) {
+//             if (column >= n - row - 1 && column <= n + row - 1) {
+//                 level += '#';
+//             } else {
+//                 level += ' ';
+//             }
+//         }
+//         console.log(level);
+//     }
+// }
+//udemy solution
+// function pyramid(n) {
+//     const midpoint = Math.floor((2 * n - 1) / 2);
+//
+//     for (let row = 0; row < n; row++) {
+//         let level = '';
+//
+//         for (let column = 0; column < (n * 2 - 1); column++) {
+//             if (midpoint - row <= column && midpoint + row >= column) {
+//                 level += '#';
+//             } else {
+//                 level += ' ';
+//             }
+//         }
+//         console.log(level);
+//     }
+// }
+//my recursive solution
+// function pyramid(n, row = 0, level = '') {
+//     const midpoint = Math.floor((2 * n - 1) / 2);
+//
+//     if (n === row) {
+//         return;
+//     }
+//     if (level.length === n * 2 - 1) {
+//         console.log(level);
+//         return pyramid(n, row + 1);
+//     }
+//     if (midpoint - row <= level.length && midpoint + row >= level.length) {
+//         level += '#';
+//     } else {
+//         level += ' ';
+//     }
+//     pyramid(n, row, level);
+// }
+//udemy's recursive solution
+function pyramid(n, row = 0, level = '') {
+    if (row === n) {
+        return;
     }
+
+    if (level.length === 2 * n - 1) {
+        console.log(level);
+        return pyramid(n, row + 1);
+    }
+
+    const midpoint = Math.floor((2 * n - 1) / 2); 
+    let add;
+    if (midpoint - row <= level.length && midpoint + row >= level.length) {
+        add = '#';
+    } else {
+        add = ' ';
+    }
+
+    pyramid(n, row, level + add);
 }
 pyramid(3);
 
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,44 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//     const vowels = 'aeiouAEIOU';
+//     const vowelArray = vowels.split('');
+//     let count = 0;
+    
+//     for (let char of str) {
+//         if (vowelArray.includes(char)) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// function vowels(str) {
+//     let count = 0;
+//     for (let char of str) {
+//         if (/^[aeiou]$/i.test(char)) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+//udemy iterative solution
+// function vowels(str) {
+//     const filter = 'aeiou';
+//     let count = 0;
 
+//     for (let char of str.toLowerCase()) {
+//         if(filter.includes(char)) {
+//             count++
+//         }
+//     }
+//     return count;
+// }
+//udemy regex solution
+function vowels (str) {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
+
+console.log(vowels('Hi There!'));
 module.exports = vowels;
