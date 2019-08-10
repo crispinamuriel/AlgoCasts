cf9f1d2c7bee7815968a46fb23c38906343ba5e9
crispinamuriel <crispinamuriel@gmail.com>
2019-08-10 16:36:52 -0400

    extra matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -127,13 +127,50 @@
 //     return results;
 // }
 
+// function matrix(n) {
+//     const results = [];
+
+//     for (let i = 0; i < n; i++){
+//         results.push([]);
+//     }
+    
+//     let counter = 1;
+//     let startColumn = 0;
+//     let endColumn = n - 1;
+//     let startRow = 0;
+//     let endRow = n - 1;
+
+//     while (startColumn <= endColumn && startRow <= endRow) {
+//         for (let i = startColumn; i <= endColumn; i++) {
+//             results[startRow][i] = counter;
+//             counter++;
+//         }
+//         startRow++
+//         for (let i = startRow; i <= endRow; i++) {
+//             results[i][endColumn] = counter;
+//             counter++;
+//         }
+//         endColumn--;
+//         for (let i = endColumn; i >= startColumn; i--) {
+//             results[endRow][i] = counter;
+//             counter++;
+//         }
+//         endRow--
+//         for (let i = endRow; i >= startRow; i--) {
+//             results[i][startColumn] = counter;
+//             counter++;
+//         }
+//         startColumn++
+//     }
+//     return results;
+// }
 function matrix(n) {
     const results = [];
 
-    for (let i = 0; i < n; i++){
+    for (let i = 0; i < n; i++) {
         results.push([]);
     }
-    
+
     let counter = 1;
     let startColumn = 0;
     let endColumn = n - 1;
@@ -155,12 +192,12 @@ function matrix(n) {
             results[endRow][i] = counter;
             counter++;
         }
-        endRow--
+        endRow--;
         for (let i = endRow; i >= startRow; i--) {
             results[i][startColumn] = counter;
             counter++;
         }
-        startColumn++
+        startColumn++;
     }
     return results;
 }
